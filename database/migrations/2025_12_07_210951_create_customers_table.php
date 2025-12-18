d30f3db8183fdd156d3cfda4e23d4562570f5cd3
Charles Chadwick <charles@mutuallyassured.com>
2025-12-18 06:07:55 -0800

    Removing unique constraint from email for now

--- database/migrations/2025_12_07_210951_create_customers_table.php
+++ database/migrations/2025_12_07_210951_create_customers_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('suffix', 15)->nullable();
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
